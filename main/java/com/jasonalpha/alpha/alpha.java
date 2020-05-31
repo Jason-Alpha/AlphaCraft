845e913982d27925afbe8ab232dc834c4b5e8a27
JasonAlpha-MODDER <azzza17317574852@qq.com>
2020-05-31 19:26:50 +0800

    alphamod

---
+++ main/java/com/jasonalpha/alpha/alpha.java
@@ -0,0 +1,38 @@
+package com.example.examplemod;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Alpha.alpha, version = alpha.0.0.1)
+public class alpha
+{
+    public static final String MODID = "alpha";
+    public static final String VERSION = "0.0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.func_149730_j());
+    }
+}
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
